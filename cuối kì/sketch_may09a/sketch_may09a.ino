c404322bd8a5e75a71c4b73e761eea9dce47c749
conankundo <94632035+conankundo@users.noreply.github.com>
2024-03-31 22:50:59 +0700

    Add files via upload

---
+++ cuối kì/sketch_may09a/sketch_may09a.ino
@@ -0,0 +1,17 @@
+float pre_goc = 0;
+float goc_hientai;
+float pre_time = 0
+;
+float denta_goc(){
+  goc_hientai = tinhGoc();
+  
+}
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
